60ed9e62854b362e3b0d19c071fbb76a7743b0da
Mohammad Meraj <143806786+Meraj1312@users.noreply.github.com>
2026-08-29 19:50:43 +0530

    Update writeups.ts

--- lib/writeups.ts
+++ lib/writeups.ts
@@ -11,6 +11,11 @@ export const WRITEUP_META: WriteupMeta[] = [
         difficulty: "Easy",
     },
     {
+        slug: "openadmin",
+        os: "Linux",
+        difficulty: "Easy",
+    },
+    {
         slug: "ten",
         os: "Linux",
         difficulty: "Hard",
